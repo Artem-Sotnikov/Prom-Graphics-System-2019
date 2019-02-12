bcd8c411e2e9664fe1280a476e3552399f437e2f
topazerino <47570422+topazerino@users.noreply.github.com>
2019-02-12 12:25:42 -0500

    Update
    regular update

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -1,4 +1,7 @@
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+// import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -6,15 +9,55 @@ public class FloorPlan extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private Display disp;
+	// private ArrayList<Table> tables;
 	
 	public FloorPlan() {
 		super("Floor Plan");
+		this.disp = new Display();
 	}
 	
+	public static void main(String[] args) {
+	}
+
 	private class Display extends JPanel {
 		private static final long serialVersionUID = 1L;
 		
 		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			setDoubleBuffered(true);
+			
+			this.repaint();
+		}
+	}
+	
+	private class MyMouseListener implements MouseListener {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
 			
 		}
 	}
