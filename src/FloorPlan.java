764983af8f8152c96191f11559805e1be321bb5a
Code-Titan <artemis.sotnikov12@gmail.com>
2019-02-13 13:01:37 -0500

    Made basic graphical framework

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
 // import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,18 +12,83 @@ public class FloorPlan extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private Display disp;
+	private ArrayList<Square> tableShapes;
+	private ArrayList<Circle> studentShapes;
+	
 	// private ArrayList<Table> tables;
 	
 	public FloorPlan() {
 		super("Floor Plan");
 		this.disp = new Display();
-		this.setSize(800,800);
+		this.add(disp);
+		
+		this.setSize(1000,1000);
+		
 		
 		MyMouseListener mouseListener = new MyMouseListener();
 		this.addMouseListener(mouseListener);
 		
 		this.requestFocusInWindow();
 		this.setVisible(true);
+		
+		tableShapes = new ArrayList<Square>(0);
+		studentShapes = new ArrayList<Circle>(0);
+	}
+	
+	public void generateFloorPlan(ArrayList<Table> tables) {
+		int tableSize = tables.get(0).getSize();
+		double determinedX = 0;
+		double determinedY = 0;
+		
+		for (int i = 0; i < tables.size(); i++) {
+			Square tableCreation = new Square();
+			tableCreation.setHeight(40);
+			tableCreation.setWidth((tableSize/2)*10);	
+			
+			
+			if (i == 0) {
+				determinedX = 100;
+				determinedY = 100;
+				System.out.println("initial active");
+			} else {
+				determinedX = tableShapes.get(i - 1).getX() + 100;
+				determinedY = tableShapes.get(i - 1).getY();
+				if (determinedX > 900 - tableSize*10) {
+					determinedX = 100;
+					determinedY = determinedY + 100;
+				}
+				
+																		
+			}
+			
+			tableCreation.setX(determinedX);
+			tableCreation.setY(determinedY);
+			
+			tableShapes.add(tableCreation);
+			
+		}
+	}
+	
+	private class LoadFile {
+		public ArrayList<Square> tableList;
+		public ArrayList<Circle> studentList;
+		
+		LoadFile(ArrayList<Square> tableL,ArrayList<Circle> studentL) {
+			this.studentList = studentL;
+			this.tableList = tableL;
+		}
+		
+		//temporary, do not use in final version
+		LoadFile() {}
+		
+	}
+	
+	private LoadFile loadShapes() {		
+		return new LoadFile();		
+	}
+	
+	private void saveShapes() {
+		// write to file this.shapes		
 	}
 	
 	public void displayFloorPlan() {
@@ -38,6 +105,14 @@ public class FloorPlan extends JFrame {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			setDoubleBuffered(true);
+			
+			for (int i = 0; i < studentShapes.size(); i++) {
+				studentShapes.get(i).draw(g);				
+			}
+			
+			for (int i = 0; i < tableShapes.size(); i++) {
+				tableShapes.get(i).draw(g);
+			}
 			
 			this.repaint();
 		}
@@ -75,7 +150,7 @@ public class FloorPlan extends JFrame {
 		}
 	}
 	
-	private class Shape {
+	private abstract class Shape {
 		private double x;
 		private double y;
 		
@@ -83,6 +158,11 @@ public class FloorPlan extends JFrame {
 			this.x = x;
 			this.y = y;
 		}
+		
+		public Shape() {};
+		
+		abstract void draw(Graphics g); 
+		
 
 		/**
 		 * @return the x
@@ -123,10 +203,58 @@ public class FloorPlan extends JFrame {
 			this.height = height;
 		}
 		
+		public Square() {};
+		
+		public double getWidth() {
+			return width;
+		}
+
+		public void setWidth(double width) {
+			this.width = width;
+		}
+
+		public double getHeight() {
+			return height;
+		}
+
+		public void setHeight(double height) {
+			this.height = height;
+		}
+
+		
+		@Override
 		public void draw(Graphics g) {
-			g.setColor(Color.BLUE);
+			Color IP_PURPLE = new Color(135,128,184);
+			g.setColor(IP_PURPLE);
 			g.fillRect((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
 		}
+	}
+	
+	private class Circle extends Shape {
+		private double radius;
+		
+		public Circle(double x, double y, double rad) {
+			super(x, y);
+			this.radius = rad;
+			// TODO Auto-generated constructor stub
+		}
+		
+		public Circle() {};
+		
+		public double getRadius() {
+			return this.radius;
+		}
+		
+		public void setRadius(double rad) {
+			this.radius = rad;
+		}
+		
+		@Override
+		public void draw(Graphics g) {
+			g.setColor(Color.BLUE);
+			g.fillOval((int)this.getX(),(int)this.getY(),(int)this.radius,(int)this.radius);
+		}
+		
 	}
 	
 	public static void main(String[] args) {
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,9 +1,21 @@
 import javax.swing.JFrame;
+import java.util.ArrayList;
 
 public class TestMain {	
-	static JFrame window;
+	static JFrame window;	
+
 	
 	public static void main(String[] args) {
 		window = new FloorPlan();
+		
+		ArrayList<Table> testTables = new ArrayList<Table>();
+		
+		for (int i = 0; i < 40; i++) {
+			testTables.add(new Table());
+		}
+		
+		((FloorPlan) window).generateFloorPlan(testTables);
 	}
+	
+	
 }
