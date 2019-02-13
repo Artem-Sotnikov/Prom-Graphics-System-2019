83aac081ed5a319303b79b8223a3b3a3fb567942
topazerino <47570422+topazerino@users.noreply.github.com>
2019-02-13 11:31:06 -0500

    update
    added some other basic private classes for the graphics

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -14,10 +15,21 @@ public class FloorPlan extends JFrame {
 	public FloorPlan() {
 		super("Floor Plan");
 		this.disp = new Display();
+		this.setSize(800,800);
+		
+		MyMouseListener mouseListener = new MyMouseListener();
+		this.addMouseListener(mouseListener);
+		
+		this.requestFocusInWindow();
+		this.setVisible(true);
 	}
 	
 	public void displayFloorPlan() {
-		
+		this.disp.repaint();
+	}
+	
+	public void exit() {
+		this.dispose();
 	}
 
 	private class Display extends JPanel {
@@ -33,7 +45,7 @@ public class FloorPlan extends JFrame {
 	
 	private class MyMouseListener implements MouseListener {
 		@Override
-		public void mouseClicked(MouseEvent arg0) {
+		public void mouseClicked(MouseEvent e) {
 			// TODO Auto-generated method stub
 			
 		}
@@ -60,6 +72,60 @@ public class FloorPlan extends JFrame {
 		public void mouseReleased(MouseEvent arg0) {
 			// TODO Auto-generated method stub
 			
+		}
+	}
+	
+	private class Shape {
+		private double x;
+		private double y;
+		
+		public Shape(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		/**
+		 * @return the x
+		 */
+		public double getX() {
+			return x;
+		}
+
+		/**
+		 * @param x the x to set
+		 */
+		public void setX(double x) {
+			this.x = x;
+		}
+
+		/**
+		 * @return the y
+		 */
+		public double getY() {
+			return y;
+		}
+
+		/**
+		 * @param y the y to set
+		 */
+		public void setY(double y) {
+			this.y = y;
+		}
+	}
+	
+	private class Square extends Shape {
+		private double width;
+		private double height;
+		
+		public Square(double x, double y, double width, double height) {
+			super(x, y);
+			this.width = width;
+			this.height = height;
+		}
+		
+		public void draw(Graphics g) {
+			g.setColor(Color.BLUE);
+			g.fillRect((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
 		}
 	}
 	
--- src/TestMain.java
+++ src/TestMain.java
@@ -5,7 +5,5 @@ public class TestMain {
 	
 	public static void main(String[] args) {
 		window = new FloorPlan();
-		window.setVisible(true);
-		window.setSize(800,800);
 	}
 }
