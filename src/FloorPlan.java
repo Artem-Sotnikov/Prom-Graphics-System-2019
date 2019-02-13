e55ff825a65654ef5f98a915780306de6002320f
topazerino <47570422+topazerino@users.noreply.github.com>
2019-02-13 12:51:41 -0500

    Update
    Added some really basic hover info.

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -1,24 +1,32 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-// import java.util.ArrayList;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+//import java.util.ArrayList;
 
 public class FloorPlan extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private Display disp;
 	// private ArrayList<Table> tables;
+	private Shape square;
+	private Shape info;
 	
 	public FloorPlan() {
 		super("Floor Plan");
 		this.disp = new Display();
-		this.setSize(800,800);
 		
-		MyMouseListener mouseListener = new MyMouseListener();
-		this.addMouseListener(mouseListener);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(this.disp);
+		this.setSize(1000,1000);
+		
+		this.square = new Square(100, 100, 50, 50);
+		this.info = new Square(150, 50, 80, 50);
 		
 		this.requestFocusInWindow();
 		this.setVisible(true);
@@ -35,47 +43,67 @@ public class FloorPlan extends JFrame {
 	private class Display extends JPanel {
 		private static final long serialVersionUID = 1L;
 		
+		private MyMouseListener mouseListener;
+		
+		public Display() {
+			this.mouseListener = new MyMouseListener();
+			this.addMouseListener(this.mouseListener);
+			this.addMouseMotionListener(this.mouseListener);
+		}
+		
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			setDoubleBuffered(true);
 			
+			Point mousePos = this.mouseListener.getPos();
+			if (square.getBoundingBox().contains(mousePos)) {
+				info.draw(g);
+			}
+			square.draw(g);
 			this.repaint();
 		}
 	}
-	
-	private class MyMouseListener implements MouseListener {
+
+	private class MyMouseListener implements MouseInputListener, MouseMotionListener {
+		private int x;
+		private int y;
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void mouseExited(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void mousePressed(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
-			
+		}
+		
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+		}
+		
+		public Point getPos() {
+			return new Point(x, y);
 		}
 	}
 	
-	private class Shape {
+	private abstract class Shape {
 		private double x;
 		private double y;
 		
@@ -92,25 +120,15 @@ public class FloorPlan extends JFrame {
 		}
 
 		/**
-		 * @param x the x to set
-		 */
-		public void setX(double x) {
-			this.x = x;
-		}
-
-		/**
 		 * @return the y
 		 */
 		public double getY() {
 			return y;
 		}
-
-		/**
-		 * @param y the y to set
-		 */
-		public void setY(double y) {
-			this.y = y;
-		}
+		
+		public abstract void draw(Graphics g);
+		
+		public abstract Rectangle getBoundingBox();
 	}
 	
 	private class Square extends Shape {
@@ -123,9 +141,15 @@ public class FloorPlan extends JFrame {
 			this.height = height;
 		}
 		
+		@Override
 		public void draw(Graphics g) {
 			g.setColor(Color.BLUE);
 			g.fillRect((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
+		}
+
+		@Override
+		public Rectangle getBoundingBox() {
+			return new Rectangle((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
 		}
 	}
 	
--- src/TestMain.java
+++ src/TestMain.java
@@ -5,5 +5,8 @@ public class TestMain {
 	
 	public static void main(String[] args) {
 		window = new FloorPlan();
+		while (true) {
+			((FloorPlan) window).displayFloorPlan();
+		}
 	}
 }
