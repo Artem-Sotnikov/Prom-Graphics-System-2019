3753dd6e1ac62aa4e769726f9fa2d73619bc592b
Code-Titan <artemis.sotnikov12@gmail.com>
2019-02-18 12:50:33 -0500

    Added archive

---
+++ 2_18_2019 src archive/src/TestMain.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import java.util.ArrayList;
+
+public class TestMain { 
+	static JFrame window; 
+
+	public static void main(String[] args) {
+		window = new FloorPlan();
+
+		ArrayList<Table> testTables = new ArrayList<Table>();
+
+		for (int i = 0; i < 20; i++) {
+			testTables.add(new Table());
+		}
+		
+		// either load from existing file OR generate from list of tables
+
+		// ((FloorPlan) window).loadShapesFromFile();
+		((FloorPlan) window).generateFloorPlan(testTables);
+
+		//while (true) {
+			((FloorPlan) window).displayFloorPlan();
+		//}
+	} 
+}
---
+++ 2_18_2019 src archive/src/constant/Constants.java
@@ -0,0 +1,16 @@
+package constant;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class Constants {
+
+	 public final static Color IP_PURPLE = new Color(135,128,184);
+	 public static final Color LIGHT_GRAY = new Color(196,196,196);
+	 public final static int SCALE_FACTOR = 20;
+	 public static final int OFFSET_FACTOR = 5;
+
+	 public final static Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();    
+	
+}
