88de5ee3464f22000464876b836491d229235324
Code-Titan <artemis.sotnikov12@gmail.com>
2019-02-19 10:32:51 -0500

    Added Archive

---
+++ 2_19_2019 src archive/src/DispRectangle.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.io.Serializable;
+
+public class DispRectangle extends Shape implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private double width;
+	private double height;
+
+	public DispRectangle(double x, double y, double width, double height) {
+		super(x, y);
+		this.width = width;
+		this.height = height;
+	}
+
+	public DispRectangle() {};
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	@Override
+	public void draw(Graphics g, Color color) {
+		g.setColor(color);
+		g.fillRect((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
+	}
+	
+	@Override 
+	public void draw(Graphics g) {
+		g.setColor(this.getPrivateColor());
+		g.fillRect((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
+	}
+
+	@Override
+	public Rectangle getBoundingBox() {
+		return new Rectangle((int)this.getX(), (int)this.getY(), (int)this.width, (int)this.height);
+	}
+	
+	@Override
+	public String toString() {
+		return "X: " + this.getX() + "\nY: " + this.getY() + "\nWidth: " + this.width + "\nHeight: " + this.height;
+	}
+}
---
+++ 2_19_2019 src archive/src/DispTable.java
@@ -0,0 +1,123 @@
+import static constant.Constants.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class DispTable extends DispRectangle {
+	private Table originalTable;
+	private boolean isRound;
+	
+	
+	public Table getOriginalTable() {
+		return originalTable;
+	}
+
+	public void setOriginalTable(Table originalTable) {
+		this.originalTable = originalTable;
+	}
+
+	DispTable() {
+		this.setHovered(false);
+		this.setHighlighted(false);
+		this.setReal(false);
+		this.setSelected(false);	
+		this.setRound(false);
+	}
+	
+	public boolean isRound() {
+		return isRound;
+	}
+
+	public void setRound(boolean isRound) {
+		this.isRound = isRound;
+	}
+
+	public void drawBox(Graphics g, int tableNumber) {
+		if (this.isReal()) {
+			DispRectangle infoBox = new DispRectangle();
+			infoBox.setX(this.getX() - SCALE_FACTOR*4 - 2);
+		    infoBox.setY(this.getY() - SCALE_FACTOR*2 - 2);
+		    infoBox.setWidth(SCALE_FACTOR*4);
+		    infoBox.setHeight(SCALE_FACTOR*2);
+		    infoBox.draw(g,Color.MAGENTA);
+		    g.setColor(Color.BLACK);
+		    g.drawString("Table " + Integer.toString(tableNumber),(int)infoBox.getX() + 5,(int)infoBox.getY() + 15);
+		}
+	}
+	
+	@Override
+	public void draw(Graphics g, Color color) {	
+		g.setColor(color);
+		
+		if (this.isRound()) {
+			g.fillOval((int)this.getX(), (int)this.getY(), (int)this.getWidth(), (int)this.getHeight());
+		} else {
+			g.fillRect((int)this.getX(), (int)this.getY(), (int)this.getWidth(), (int)this.getHeight());
+		}
+	}	
+	
+	public void drawReduced(Graphics g, Color color, int reductionSize) {
+		g.setColor(color);
+		
+		if (this.isRound()) {
+			g.fillOval((int)this.getX() + reductionSize,(int)this.getY() + reductionSize,
+					(int)this.getWidth() - reductionSize*2,(int)this.getHeight() - reductionSize*2);
+		} else {
+			g.fillRect((int)this.getX() + reductionSize,(int)this.getY() + reductionSize,
+					(int)this.getWidth() - reductionSize*2,(int)this.getHeight() - reductionSize*2);
+		}
+	}
+	
+	public void drawObject(Graphics g) {
+		if (this.isHovered()) {					
+					
+			if (this.isSelected()) {
+				this.draw(g,Color.MAGENTA);
+			} else if (this.isHighlighted()) {
+				this.draw(g,Color.GREEN);
+			} else if (this.isReal()) {
+				this.draw(g,Color.MAGENTA);
+			}
+						
+			
+			if (this.isSelected()) {
+				this.drawReduced(g,Color.YELLOW,5);
+			} else if (this.isReal()) {
+				this.drawReduced(g,IP_PURPLE,5);
+			} else {
+				this.drawReduced(g,LIGHT_GRAY,5);
+			}
+
+		
+	        if ((this.isHighlighted()) && (!this.isSelected())) {
+								
+				this.drawReduced(g,Color.GREEN,8);				
+				
+			}
+	
+		} else if (this.isSelected()) {
+			this.draw(g,Color.YELLOW);			
+		} else if (this.isHighlighted()) {
+			
+			this.draw(g,Color.GREEN);
+			
+			if (this.isReal()) {
+				this.drawReduced(g,IP_PURPLE,5);
+			} else {
+				this.drawReduced(g,LIGHT_GRAY,5);
+			}
+
+			
+		} else {
+			if (this.isReal()) {
+				this.draw(g,IP_PURPLE);
+			} else {
+				this.draw(g,LIGHT_GRAY);
+			}
+		}
+	}
+}
+
+
+
+
