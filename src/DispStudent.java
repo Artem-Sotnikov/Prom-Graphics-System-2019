d0c224de8680bbb7131be671f8ac9a3c640858e1
topazerino <47570422+topazerino@users.noreply.github.com>
2019-02-18 21:00:23 -0500

    update
    update

--- src/DispStudent.java
+++ src/DispStudent.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import static constant.Constants.*;
 
-
 public class DispStudent extends DispCircle implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
